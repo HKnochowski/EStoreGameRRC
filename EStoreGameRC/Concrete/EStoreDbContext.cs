f0f4f63f11cd9e7326ca99a1d149c8d7e48b329f
Hubert Knochowski <Knochos@gmail.com>
2018-01-18 22:29:44 +0100

    Update 1.6 HK

---
+++ EStoreGameRC/Concrete/EStoreDbContext.cs
@@ -0,0 +1,37 @@
+namespace EStoreGameRC.Concrete
+{
+    using EStoreGameRC.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class EStoreDbContext : DbContext
+    {
+        public EStoreDbContext()
+            : base("name=EStoreDbContext")
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Copie> Copies { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Gallery> Galleries { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameLanguage> GamesLanguages { get; set; }
+        public DbSet<GamePublisher> GamesPublishers { get; set; }
+        public DbSet<GameType> GamesTypes { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<PlatformType> PlatformsTypes { get; set; }
+        public DbSet<SalesInvoice> SalesInvoices { get; set; }
+        public DbSet<State> SystemRequirs { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public DbSet<WishList> WishesLists { get; set; }
+
+
+    }
+
+}
